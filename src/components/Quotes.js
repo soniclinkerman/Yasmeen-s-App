2321b4b2848f5e379874e60f22aa496b9915129a
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-03-07 20:45:55 -0500

    Add quote

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "../css/Quotes.css";
 import "../css/Music.css";
 // "Who you are, what you think, feel, and do, what you love—is the sum of what you focus on ~Gallagher",
-const quotes = [ "Life is too short, so don't spend it living someone else's life ~ Steve Jobs"]
+const quotes = [ "Never compare your beginning to someone else’s middle"]
 
 var x = Math.floor(Math.random() * quotes.length);
 var y = quotes[x];
