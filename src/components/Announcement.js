e989346b3e2da7c8d1dc561e9b1436b5325b1346
sonic <sonic@Isaiahs-MacBook-Pro.local>
2020-03-07 20:46:33 -0500

    Shorten button alert

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -16,29 +16,29 @@ class Announcements extends Component{
     }
 
     feat(){
-        alert(
-            "Hello, it's nice to meet you! I'm Yazzle!"
-        )
+        // alert(
+        //     "Hello, it's nice to meet you! I'm Yazzle!"
+        // )
 
-        alert(
-            "I'll be updated from time to time and all you need to do is click this little button to see what's new about me!"
-        )
+        // alert(
+        //     "I'll be updated from time to time and all you need to do is click this little button to see what's new about me!"
+        // )
 
-        alert(
-            "I might not have everything you want in me now, but that's where the fun comes in! You get to watch me grow!"
-        )
+        // alert(
+        //     "I might not have everything you want in me now, but that's where the fun comes in! You get to watch me grow!"
+        // )
 
-        alert(
-            "If you're your not sure what you wanted added, how about suggesting a new icon for your app. While the current one isn't bad, it's not exactly square..."
-        )
+        // alert(
+        //     "If you're your not sure what you wanted added, how about suggesting a new icon for your app. While the current one isn't bad, it's not exactly square..."
+        // )
 
-        alert(
-            "Once you turn that image into a square on another software or prefer to use a different image, let 'you know who' about it and check back later!"
-        )
+        // alert(
+        //     "Once you turn that image into a square on another software or prefer to use a different image, let 'you know who' about it and check back later!"
+        // )
 
-        alert(
-            "If you have any questions or want features added/removed, you know who to talk to (◕‿◕✿"
-        )
+        // alert(
+        //     "If you have any questions or want features added/removed, you know who to talk to (◕‿◕✿"
+        // )
 
         alert(
             "Date: " + [announcment[0].date] + "\r\n" +
